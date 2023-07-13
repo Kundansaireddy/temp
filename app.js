df0ae91ae8c6bdca061af99317a975a2adcffe57
Kundan Sai Reddy <81676583+Kundansaireddy@users.noreply.github.com>
2023-07-13 17:21:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("App is running succesfully");
+});
+app.listen(8080, console.log("Server running"));
